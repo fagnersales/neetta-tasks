ffb1576dfd441d560743b0a6a08e2baba670fb8e
Fagner Sales <fs.fagnersales@gmail.com>
2021-04-05 09:40:45 -0300

    Added property fulfilled

--- src/commands/Reminder/Read/ReminderEditUI.ts
+++ src/commands/Reminder/Read/ReminderEditUI.ts
@@ -8,14 +8,14 @@ moment.locale('pt-br')
 
 export const createEditPage = ({ what, at }: Reminder): Page => {
   const editPage = new Page([
-    { key: 'what', name: 'Oque', instanceOf: 'text', value: what, isRequired: true },
-    { key: 'at', name: 'tempo', instanceOf: 'text', value: moment(+at).fromNow(), isRequired: true, setter: (value) => {
+    { key: 'what', name: 'Oque', instanceOf: 'text', value: what, fulfilled: true },
+    { key: 'at', name: 'tempo', instanceOf: 'text', value: moment(+at).fromNow(), fulfilled: true, setter: (value) => {
       const hasSMDChar = /[smd]/.test(value)
       if (!hasSMDChar) return false      
       return !isNaN(ms(value))
       }
     },
-    { key: 'repeatDaily', name: 'diariamente', instanceOf: 'S/N', value: 'Você quer que este lembrete repita diariamente?', isRequired: true }
+    { key: 'repeatDaily', name: 'diariamente', instanceOf: 'S/N', value: 'Você quer que este lembrete repita diariamente?', isRequired: false }
   ], {
     stilize: (data) => {
       const stilize = stilizeReminderUI(editPage, data)
